11b74b384eb1a40a335cad8219a333b3909fd0ab
NidRzk <arthurnunessilva0810@gmail.com>
2025-10-20 17:25:30 -0300

    primeiro commit

---
+++ aula12.js
@@ -0,0 +1,51 @@
+// Comentarios
+
+/*
+    Comentario
+    de
+    multiplas 
+    linhas
+*/
+
+// Variavel no java
+
+// Uma variavel é um espaço na memoria do computador que recebe um valor.
+// Esse valor pode ser lido ou alterado a qualquer momento.
+
+let numero = 5
+
+const nome = "Gustavo";
+
+// let => Pode ter o seu valor alterado.
+// const => Nao pode ter o seu valor alterado.
+
+console.log(nome)
+
+// DOM -> Document Object Model
+
+document.writeln('<h1>Hello, World</h1>')
+
+// document.body.innerHTML = ""
+
+// Tipos de dados
+
+// str - cadeia de caracteres
+// int - numeros inteiros
+// float - numeros com casas decimais
+// boolean - tipo logico / vdd ou falso
+
+// null - nulo
+// underfined - indefinido
+// NaN - Not a Number
+
+// Iniciando umna variavel vazia
+let teste;
+
+// Number()
+
+let idade = 18;
+
+console.log(typeof(idade))
+
+const exemplo = prompt('Digite seu nome')
+console.log('Ola' + exemplo)
