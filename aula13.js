9c5b4dca87366efba4ca06e617513fcb718981b7
NidRzk <arthurnunessilva0810@gmail.com>
2025-11-03 15:37:37 -0300

    first commit

---
+++ aula13.js
@@ -0,0 +1,47 @@
+// 1 - nao cmc com numeros
+// 2 - nao conter simbolos
+// 3 - nao ter espaços
+
+// Condicionais
+
+//if (condicao){
+
+//} else {
+
+//}
+
+// Operadores logicos
+
+
+// >, <
+// >=
+// <=
+// == / ===
+// != / !==
+
+const gustavoNig = 10;
+const pacheco = 5;
+
+if(gustavoNig >= pacheco){
+    console.log("Gustavo é maior q o Pacheco")
+} else {
+    console.log("Gustavo é menor q o Pacheco")
+}
+
+// Listas
+
+const listaDecores = ['Verde', 'Amarelo', 'Azul']
+
+console.log(listaDecores[1])
+
+// Python -> JS
+
+// len() - length()
+// append() - push()
+
+// Array multidimensional
+
+const times = [['Ruan', 'Flamengo'], ['Arthur', 'Real']]
+console.log(times[1])
+
+// + - / // * ** %
